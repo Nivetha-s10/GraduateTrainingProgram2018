02027620475a686d7e88b478bafb56a4b702be08
Nivetha-s10 <41381977+Nivetha-s10@users.noreply.github.com>
2018-07-25 09:08:07 +0530

    Create Solutions.sql

---
+++ SQL/ProblemSet00/Solutions.sql
@@ -0,0 +1,15 @@
+-- ProblemSet 00,July 25 2018
+-- Submission by nivetha.seenivasan 
+
+1)Select the Employee with the top three salaries
+
+select * from employee order by salary desc limit 3;
+
+A142|Tara Cummings|D04|99475|A187
+A132|PAUL VINCET|D01|94791|A120
+A128|Adam Wayne|D05|94324|A165
+
+record count- 3
+
+
+
